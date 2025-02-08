932e797f42869763ec30cfb5e7fabc4e89a2da46
curtisim0 <curtis@ross.place>
2025-02-07 21:38:01 -0600

    Completes multi-build steps of tutorial
    HOWEVER, this is completely broken.
    
    Some patterns for "react native" that I am unused to and I borked
    something.

---
+++ app.config.ts
@@ -0,0 +1,80 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.curtisim0.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.curtisim0.stickersmash.preview';
+  }
+
+  return 'com.curtisim0.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+export default {
+  name: getAppName(),
+  slug: "stickersmash",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: getUniqueIdentifier(),
+    infoPlist: {
+      ITSAppUsesNonExemptEncryption: false
+    }
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    package: getUniqueIdentifier()
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  },
+  extra: {
+    router: {
+      origin: false
+    },
+    eas: {
+      projectId: "ff355b37-d631-4296-a73d-7b4bc226b71a"
+    }
+  },
+  owner: "curtisim0"
+};
